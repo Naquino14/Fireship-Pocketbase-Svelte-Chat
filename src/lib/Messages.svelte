07c695226561ba460e63c0e00cb239c0fa6e3964
Nathaniel Aquino <naquino14@outlook.com>
2022-12-20 12:27:09 -0500

    Real time messaging!

--- src/lib/Messages.svelte
+++ src/lib/Messages.svelte
@@ -5,6 +5,8 @@
 
   let messages = [];
 
+  let unsubscribe: () => void;
+
   // onmount is called at the beginning of the component lifecycle
   onMount(async () => {
     console.log("mounted");
@@ -15,10 +17,48 @@
       expand: "user", // expand allows us to include user data relationally to the message
     });
     messages = resultList.items;
+
+    // subscribe to realtime messages
+    unsubscribe = await pb
+      .collection("messages")
+      .subscribe("*", async ({ action, record }) => {
+        try {
+          if (action === "create") {
+            // fetch the user associated with the message
+            const user = await pb.collection("users").getOne(record.user);
+            record.expand = { user };
+            messages = [...messages, record];
+          } else if (action === "delete") {
+            messages.filter((m: { id: string }) => m.id !== record.id);
+          }
+        } catch (error) {
+          if ((error.message as string).includes("autocancelled"))
+            console.info("Request autocancelled.");
+          else console.error("error!", error.message);
+        }
+      });
+  });
+
+  let message: string;
+  const sendMessage = async () => {
+    const data = {
+      text: message,
+      user: $currentUser.id,
+    };
+    await pb.collection("messages").create(data);
+  };
+
+  onDestroy(() => {
+    console.log("destroyed");
+    unsubscribe?.();
   });
 </script>
 
 {#if $currentUser}
+  <form on:submit|preventDefault={sendMessage}>
+    <input bind:value={message} placeholder="Message" type="text" />
+    <button on:submit={() => (message = "")} type="submit">Send</button>
+  </form>
   <div class="messages">
     {#each messages as message (message.id)}
       <div class="message">
